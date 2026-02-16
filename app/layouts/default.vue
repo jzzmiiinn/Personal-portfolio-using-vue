254a1661f0471b928f662f89ea05aefce30fca68
jzzmiiinn <jzzmiiinnali@gmail.com>
2026-02-17 00:16:02 +0300

    Fix build error and update project files2

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script setup>
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
+import Navbar from '../components/Navbar.vue'
+import Footer from '../components/Footer.vue'
 </script>
 
 <style scoped>
